918c64fb2776201964cfdc7b0a51fcba8c1c15ff
Benjamin Leonhardt <benjamin.leonhardt@lyth.de>
2021-02-05 15:00:36 +0100

    First commit

---
+++ vehicles-api/src/main/resources/data.sql
@@ -0,0 +1,14 @@
+INSERT INTO car (id, condition) VALUES (1,'USED');
+INSERT INTO car (id, condition) VALUES (2,'USED');
+INSERT INTO car (id, condition) VALUES (3,'USED');
+INSERT INTO car (id, condition) VALUES (4,'USED');
+INSERT INTO car (id, condition) VALUES (5,'USED');
+INSERT INTO car (id, condition) VALUES (6,'USED');
+INSERT INTO car (id, condition) VALUES (7,'USED');
+INSERT INTO car (id, condition) VALUES (8,'USED');
+INSERT INTO car (id, condition) VALUES (9,'USED');
+INSERT INTO car (id, condition) VALUES (10,'USED');
+
+
+
+
